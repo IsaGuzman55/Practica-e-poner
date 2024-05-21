71ab05a900dd9efa681e12ac8d0cb49bdf592163
IsaGuzman55 <isabellaguzman143@gmail.com>
2024-05-21 16:50:54 -0500

    Instalar el gitignore

---
+++ SQL/BaseDatos.sql
@@ -0,0 +1,8 @@
+-- Active: 1716323923095@@bwcsz1zo9o3rlrkn9kjp-mysql.services.clever-cloud.com@3306
+CREATE TABLE Customers(
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(255),
+    LastName VARCHAR(255),
+    DocumentNumber VARCHAR(255)
+);
+
